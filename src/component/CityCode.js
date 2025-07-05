1a83b36df2a045295a982b95416993b798f75473
JiRungYeeOwO <hosik9398@gmail.com>
2025-07-05 18:53:20 +0900

    Update CityCode.js

--- src/component/CityCode.js
+++ src/component/CityCode.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default class CityCode {
   constructor() {
-    this.apikey = "NM515P%2FA6e%2FDbfiSxQzD8w1KcctUqklBzWF0yxB56Te%2Fsfr3UYtvU0jePBzC6s3d8jVe%2BKcZ6smV%2BywkRmBbow%3D%3D";
+    this.apikey = process.env.REACT_APP_API_KEY;
     this.apiUrl = "http://apis.data.go.kr/1613000/ArvlInfoInqireService/getCtyCodeList";
   }
 
